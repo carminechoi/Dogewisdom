4d339ebc2346360aa71e36bd0b6b9561d789e306
Carmine Choi <carmine.choi@gmail.com>
2021-11-10 16:24:51 -0800

    client successfully calls server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { React, useState, useEffect } from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +8,10 @@ function App() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch('/api')
-          .then((res) => res.json())
-          .then((data) => setData(data.message));
-    }, []);
+        axios.get('http://localhost:5000/api').then((response) => {
+            setData(response.data.api);
+        });
+      }, []);
 
     return (
         <div className="App">
