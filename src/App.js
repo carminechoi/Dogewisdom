67a358849a52564b06176870568228d1753dcd8c
Carmine Choi <carmine.choi@gmail.com>
2021-11-11 10:35:46 -0800

    setting dev and prod server link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 
+import { config } from './Constants'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const url = config.url.API_URL;
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api').then((response) => {
+        axios.get(url).then((response) => {
             setData(response.data.api);
         });
       }, []);
