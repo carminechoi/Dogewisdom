1cda444b553d2e5a4854cf1098393715d0a47e30
Carmine Choi <carmine.choi@gmail.com>
2021-11-11 16:30:34 -0800

    testing database

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ app.get('/api', (req, res) => {
     res.status(200).json({api: 'hello from server!'});
 })
 
+app.get('/database', (req, res) => {
+    res.status(200).json({dbData: 'hello from server!'});
+})
+
 app.listen(port, () => console.log(`server started on ${port}`));
