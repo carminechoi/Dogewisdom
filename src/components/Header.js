715245bb6037ab8b2bc717ac20845b77d2cd8483
Carmine <carmine.choi@gmail.com>
2021-12-05 23:32:28 -0800

    header outline

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../assets/doge-logo.png';
+import './Header.css';
 
 function Header() {
     return (
-        <div className="Header">
+        <header className="header-container">
+            <Link to="/" className="title-container">
+                <img src={logo} className="title-logo title-container-item" alt="logo" width="75" height="75" />
+                <div className="title-container-item">
+                    <h1 className="title-heading">DogeWisdom.</h1>
+                    <h2 className="title-subheading">Reddit Crypto Tracker</h2>
+                </div>
+            </Link>
+            <div className="search-container">
+
+            </div>
+            <div className="account-options-container">
+
+            </div>
             <p>Header</p>
-        </div>
+        </header>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 
 function Home() {
     return (
-        <div className="HomePage">
+        <div className="homepage">
             <Header/>
         </div>
     )
