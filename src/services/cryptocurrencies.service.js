5c4f1170322bffcbf905e75c43aecbf3cce1ec76
Carmine Choi <carmine.choi@gmail.com>
2021-12-08 11:09:33 -0800

    axios async issue resolved

--- src/services/cryptocurrencies.service.js
+++ src/services/cryptocurrencies.service.js
@@ -5,13 +5,11 @@ import axios from 'axios';
 class CryptocurrenciesService {
 	static async getAll() {
 		try  {
-			await axios.get('https://apewisdom.io/api/v1.0/filter/all-crypto/page/1')
-			.then ( (response) => {
-				console.log(response.data);
-				return response.data;
-			});
+			const url = 'https://apewisdom.io/api/v1.0/filter/all-crypto/page/1';
+			const response = await axios.get(url);
+			return response.data.results;
 		} catch (e) {
-			throw Error('Error getting all from ape');
+			throw Error(`CryptocurrenciesService getAll: ${e}`);
 		}
 	}
 }
