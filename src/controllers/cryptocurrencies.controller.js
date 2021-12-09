14f77daafd3517e26f6dc3f143196a904a6810af
Carmine Choi <carmine.choi@gmail.com>
2021-12-08 16:11:02 -0800

    removed unused functions

--- src/controllers/cryptocurrencies.controller.js
+++ src/controllers/cryptocurrencies.controller.js
@@ -1,16 +1,5 @@
 import { CryptocurrenciesService } from '../services';
 
-
-// const getAll = async function (req, res, next) {
-//     try {
-//         let result = await CryptocurrenciesService.getAll()
-//         return res.status(200).json({ status: 200, data: result, message: "Succesfully Retrieved" });
-
-//     } catch (e) {
-//         return res.status(400).json({ status: 400, message: e.message });
-//     }
-// }
-
 class CryptocurrenciesController {
     static getAll = async function (req, res, next) {
         try {
--- src/routes/cryptocurrencies.route.js
+++ src/routes/cryptocurrencies.route.js
@@ -6,6 +6,4 @@ const router = Router();
 
 router.get('/', CryptocurrenciesController.getAll);
 
-// router.get('/:id', CryptocurrenciesController.get);
-
 export { router as cryptocurrenciesRouter };
