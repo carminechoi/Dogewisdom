ecd7afa679f1e7a2ebd68edbb5cdaa6a3c58268c
Carmine Choi <carmine.choi@gmail.com>
2022-01-07 16:26:51 -0800

    Navbar Collapse implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,48 @@
 import React from 'react';
+import { Button, Form, FormControl } from 'react-bootstrap';
 // import { Link } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';import logo from '../assets/doge-logo.png';
+import Navbar from 'react-bootstrap/Navbar';
+
+import logo from '../assets/doge-logo.png';
 import './Navbar.css';
 
 
 function Header() {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">
-                <div className="ms-5 px-2 d-flex flex-row align-items-center">
-                    <div className="">
-                        <img 
-                            src={logo}
-                            alt="logo" 
-                            width="70" 
-                            height="70"
-                            className=""
-                        />
+            <div className="d-flex me-auto">
+                <Navbar.Brand href="/">
+                    <div className="ms-5 px-2 d-flex flex-row align-items-center">
+                        <div className="">
+                            <img 
+                                src={logo}
+                                alt="logo" 
+                                width="70" 
+                                height="70"
+                                className=""
+                            />
+                        </div>
+                        <div className="title d-flex flex-column px-2">
+                            <h1 className="title-main fw-bold fs-2">DogeWisdom.</h1>
+                            <h2 className="title-secondary fs-6">Reddit Crypto Tracker</h2>
+                        </div>
                     </div>
-                    <div className="title d-flex flex-column px-2">
-                        <h1 className="title-main fw-bold fs-2">DogeWisdom.</h1>
-                        <h2 className="title-secondary fs-6">Reddit Crypto Tracker</h2>
-                    </div>
-                </div>
-            </Navbar.Brand>
-
+                </Navbar.Brand>
+            </div>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" className="me-5 px-2" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                        <Form className="">
+                            <FormControl 
+                                type="search"
+                                placeholder="Search all assets..."
+                                className=""
+                                aria-label="Search"
+                            />
+                        </Form>
+                        <Button className="" >Log in</Button>
+                        <Button className="" >Sign up</Button>
+                </Navbar.Collapse>
+            
         </Navbar>
     )
 }
