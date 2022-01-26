94130fa9add71045add73919a46ce24d75bb523a
Carmine Choi <carmine.choi@gmail.com>
2022-01-26 10:08:58 -0800

    Axios route calls crypto data from backend

--- src/components/Cards/CryptoTable.js
+++ src/components/Cards/CryptoTable.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import CardHeader from './CryptoHeader';
 import CryptoCard from './CryptoCard';
 import './CryptoTable.css'
 
 function CryptoTable () {
+    const [cryptoData, setCryptoData] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/cryptocurrencies/').then((response) => {
+            setCryptoData(response.data.data);
+        });
+    }, []);
+
     return (
         <Table className="crypto-table container table-borderless" >
             <thead>
                 <CardHeader />
             </thead>
             <tbody>
-                <CryptoCard rank="1" name="Bitcoin" symbol="BTC" mentions="1234" upvotes="1234567" />
-                <CryptoCard rank="2" name="Cardano" symbol="ADA" mentions="1234" upvotes="1234567" />
+                {cryptoData.map((data, id) => {
+                    return <CryptoCard 
+                        rank={data.rank} 
+                        name={data.name} 
+                        symbol={data.ticker.slice(0,-2)}
+                        mentions={data.mentions} 
+                        upvotes={data.upvotes}
+                    />
+                })}
             </tbody>
         </Table>
     );
