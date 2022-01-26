2328c7338073f5bdeb36359c9b69a1444d3fd3ee
Carmine Choi <carmine.choi@gmail.com>
2022-01-25 16:17:41 -0800

    Set App Background Color

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-
+import './App.css';
 
 // import { config } from './Constants'
 
 function App() {
 
     return (
-        <div className="bg-light vh-100" >
+        <div className="app-router vh-100" >
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
