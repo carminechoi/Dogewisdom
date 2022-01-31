ffd6a644f82e567e991f3ef6a59a31974cb4a976
Carmine Choi <carmine.choi@gmail.com>
2022-01-30 16:46:48 -0800

    Added remaining subreddit routes

--- src/controllers/cryptocurrencies.controller.js
+++ src/controllers/cryptocurrencies.controller.js
@@ -20,6 +20,66 @@ class CryptocurrenciesController {
             return res.status(400).json({ status: 400, message: e.message });
         }
     }
+
+    static getCryptocurrencies = async function (req, res, next) {
+        try {
+            let result = await CryptocurrenciesService.getCryptocurrencies()
+            return res.status(200).json({ status: 200, data: result, message: "Succesfully Retrieved" });
+
+        } catch (e) {
+            return res.status(400).json({ status: 400, message: e.message });
+        }
+    }
+
+    static getBitcoin = async function (req, res, next) {
+        try {
+            let result = await CryptocurrenciesService.getBitcoin()
+            return res.status(200).json({ status: 200, data: result, message: "Succesfully Retrieved" });
+
+        } catch (e) {
+            return res.status(400).json({ status: 400, message: e.message });
+        }
+    }
+
+    static getSatoshiStreetBets = async function (req, res, next) {
+        try {
+            let result = await CryptocurrenciesService.getSatoshiStreetBets()
+            return res.status(200).json({ status: 200, data: result, message: "Succesfully Retrieved" });
+
+        } catch (e) {
+            return res.status(400).json({ status: 400, message: e.message });
+        }
+    }
+
+    static getCryptoMoonShots = async function (req, res, next) {
+        try {
+            let result = await CryptocurrenciesService.getCryptoMoonShots()
+            return res.status(200).json({ status: 200, data: result, message: "Succesfully Retrieved" });
+
+        } catch (e) {
+            return res.status(400).json({ status: 400, message: e.message });
+        }
+    }
+
+    static getEthTrader = async function (req, res, next) {
+        try {
+            let result = await CryptocurrenciesService.getEthTrader()
+            return res.status(200).json({ status: 200, data: result, message: "Succesfully Retrieved" });
+
+        } catch (e) {
+            return res.status(400).json({ status: 400, message: e.message });
+        }
+    }
+
+    static getEthereum = async function (req, res, next) {
+        try {
+            let result = await CryptocurrenciesService.getEthereum()
+            return res.status(200).json({ status: 200, data: result, message: "Succesfully Retrieved" });
+
+        } catch (e) {
+            return res.status(400).json({ status: 400, message: e.message });
+        }
+    }
 }
 
 
--- src/services/cryptocurrencies.service.js
+++ src/services/cryptocurrencies.service.js
@@ -22,6 +22,66 @@ class CryptocurrenciesService {
 			throw Error(`CryptocurrenciesService getAll: ${e}`);
 		}
 	}
+
+	static async getCryptocurrencies() {
+		try  {
+			const url = 'https://apewisdom.io/api/v1.0/filter/CryptoCurrencies/page/1';
+			const response = await axios.get(url);
+			return response.data.results;
+		} catch (e) {
+			throw Error(`CryptocurrenciesService getAll: ${e}`);
+		}
+	}
+
+	static async getBitcoin() {
+		try  {
+			const url = 'https://apewisdom.io/api/v1.0/filter/Bitcoin/page/1';
+			const response = await axios.get(url);
+			return response.data.results;
+		} catch (e) {
+			throw Error(`CryptocurrenciesService getAll: ${e}`);
+		}
+	}
+
+	static async getSatoshiStreetBets() {
+		try  {
+			const url = 'https://apewisdom.io/api/v1.0/filter/SatoshiStreetBets/page/1';
+			const response = await axios.get(url);
+			return response.data.results;
+		} catch (e) {
+			throw Error(`CryptocurrenciesService getAll: ${e}`);
+		}
+	}
+
+	static async getCryptoMoonShots() {
+		try  {
+			const url = 'https://apewisdom.io/api/v1.0/filter/CryptoMoonShots/page/1';
+			const response = await axios.get(url);
+			return response.data.results;
+		} catch (e) {
+			throw Error(`CryptocurrenciesService getAll: ${e}`);
+		}
+	}
+
+	static async getEthTrader() {
+		try  {
+			const url = 'https://apewisdom.io/api/v1.0/filter/EthTrader/page/1';
+			const response = await axios.get(url);
+			return response.data.results;
+		} catch (e) {
+			throw Error(`CryptocurrenciesService getAll: ${e}`);
+		}
+	}
+
+	static async getEthereum() {
+		try  {
+			const url = 'https://apewisdom.io/api/v1.0/filter/Ethereum/page/1';
+			const response = await axios.get(url);
+			return response.data.results;
+		} catch (e) {
+			throw Error(`CryptocurrenciesService getAll: ${e}`);
+		}
+	}
 }
 
 export { CryptocurrenciesService };
