f6c1c1b813add678f7ed10b8c0553207888f3d15
carminechoi <carmine.choi@gmail.com>
2022-02-22 13:15:38 -0800

    Updated cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: ['https://dogewisdom.netlify.app'],
+    origin: ['https://dogewisdom.netlify.app', 'http://localhost:3000'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
