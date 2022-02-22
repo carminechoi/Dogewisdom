cb3dbebe16997e089ce35fca54de67e5c08264d4
carminechoi <carmine.choi@gmail.com>
2022-02-22 11:24:30 -0800

    Register correctly displays server error

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Form, Button, InputGroup } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import Cookies from "js-cookie";
 import axios from "axios";
 import { config } from '../../Constants';
@@ -13,8 +13,8 @@ function Register(props) {
     const [form, setForm] = useState({});
     const [errors, setErrors] = useState({});
 
+    // reset form before closing modal
     const onClose = props.onClose;
-
     const closeModal = (props) => {
         setForm({});
         setErrors({});
@@ -27,24 +27,69 @@ function Register(props) {
         });
     }
 
-    const findErrors = () => {
+    const setResponseErrors = (response) => {
+        console.log(response);
+        Object.keys(response).forEach(key => {
+            console.log(`key: ${key}  :  value: ${response[key]}`);
+            setErrors({...errors, 
+                [key]: response[key],
+            });
+        })
+        console.log(errors);
+    }
+
+    const findFormErrors = () => {
         const { username, email, password, password2 } = form;
         const newErrors = {};
 
         if (!username || username === "") newErrors.username = 'Enter username';
+        
         if (!email || email === "") newErrors.email = "Enter email";
+        
         if (!password || password === "") newErrors.password = "Enter a password";
         else if (!password2 || password === "") newErrors.password2 = "Confirm your password";
         else if (password !== password2) newErrors.password2 = "Those passwords didn't match. Try again.";
+        
         return newErrors;
     }
 
     const handleRegister = async (e) => {
         e.preventDefault();
+        const newErrors = findFormErrors();
 
-        const newErrors = findErrors();
-
-        setErrors(newErrors);
+        if (Object.keys(newErrors).length !== 0) {
+            setErrors(newErrors);
+        } else {
+            const url = config.url.API_URL;
+            const registerUser = {
+                username: form.username,
+                email: form.email,
+                password: form.password,
+            }
+            axios.post(url + "/api/register", registerUser, { "Content-Type": "application/json" })
+                .then(res => {
+                    //Cookies.set("token", res.data.token, { expires: 7 })
+                    //window.location.reload(false) // Reloads page for app the render again
+                    closeModal() //Closes Login Modal
+                    props.redirectTotalPage()
+                    window.location.reload(false) // Reloads page for app the render again
+                    console.log('sent')
+                })
+                .catch(err => {
+                    setResponseErrors(err.response.data.message);
+                    // if (message.email) {
+                    //     this.setState({
+                    //         duplicateEmail: true,
+                    //     })
+                    // } 
+                    // if (message.username) {
+                    //     this.setState({
+                    //         duplicateUsername: true,
+                    //     })
+                    // }
+                    // console.log(`Registration Error| email: ${message.email}, username: ${message.username}`)
+                })
+        }
         // const url = config.url.API_URL;
         // const form = e.target;
         // console.log(e.currentTarget)
